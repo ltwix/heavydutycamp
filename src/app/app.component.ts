8a5660435bab8c7613f8dee2eca917ec61977e1f
ltwix <letrix321@gmail.com>
2024-02-19 03:54:50 -0500

    feat: integrar wallet connect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'hdbootcamp-root',
   template: `
     <header>
-      <h1>Hola, soy Bob.</h1>
+      <h1>Conecta tu wallet para ver tu saldo</h1>
+
+      <hd-wallet-multi-button></hd-wallet-multi-button>
     </header>
   `,
 })
